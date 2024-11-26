740b8e3ec496807e66d911384d309a56073aef42
O-keita <o.keita@alustudent.com>
2024-11-26 11:41:55 +0000

    routes connected

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,18 @@ def dashboard():
 @main.route('/admin')
 def admin():
     return render_template('admin_center/admin.html')
+
+@main.route('/about')
+def about():
+    return render_template('home/about.html')
+@main.route('/courses')
+def courses():
+    return render_template('home/courses.html')
+
+@main.route('/services')
+def services():
+    return render_template('home/services.html')
+
+
+
+
